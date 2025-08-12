35098573fe5916ccd76480947984e274a29d38fa
Rayana <rayanatxr@gmail.com>
2025-08-12 15:48:40 -0300

    [refactor] Atualiza rotas

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,26 @@
 import fastify from 'fastify';
 import clientsRoutes from './modules/clients/routes';
 import { authPlugin } from './middleware/auth';
+import goalRoutes from "./modules/goals/routes";
+import eventRoutes from './modules/events/routes';
+import simulationRoutes from './modules/simulations/routes';
+import walletRoutes from './modules/wallet/routes';
 
 const app = fastify();
 
-// Adiciona suporte ao JSON Web Token e ao campo `user`
 app.register(authPlugin);
-
-// Registra as rotas de clientes com prefixo
 app.register(clientsRoutes, { prefix: '/clients' });
+app.register(walletRoutes, { prefix: '/wallets' });
+app.register(eventRoutes, { prefix: '/events' });
+app.register(goalRoutes, { prefix: '/goals' }); 
+app.register(simulationRoutes, { prefix: '/simulations' }); 
 
 const start = async () => {
   try {
     await app.listen({ port: 3000 });
-    console.log('🚀 Servidor rodando na porta 3000');
+    console.log('Servidor rodando na porta 3000');
   } catch (err) {
-    console.error('❌ Erro ao iniciar o servidor:', err);
+    console.error('Erro ao iniciar o servidor:', err);
     process.exit(1);
   }
 };
